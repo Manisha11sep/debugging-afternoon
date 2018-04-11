9fec0596b102baa6e78a47eaf03ffbe98a08306f
Manisha11sep <er.manisha11sep@gmail.com>
2018-04-10 20:00:33 -0700

    Debugged map function to display items

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     this.setState({
       shoppingCart: [...this.state.shoppingCart, product]
     })
-    console.log(this.state.shoppingCart);
+    console.log("items in cart ",this.state.shoppingCart);
   }
 
   removeFromShoppingCart(product) {
